11a719ef18fb5baab82dee80624bd1a191b703e4
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-07-16 23:53:54 +0300

    delete unused

--- src/js/components/refs.js
+++ src/js/components/refs.js
@@ -1,7 +1,5 @@
 export default {
     searchForm: document.querySelector('#search-form'),
-    searchInput:document.querySelector('#search-form input'),
-    searchButton:document.querySelector('#search-form button'),
     loadMoreButton: document.querySelector('.load-more'),
     gallery: document.querySelector('.gallery')
 };
